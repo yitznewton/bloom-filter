3f26d20b051ec0f373be6c6be10444135543d19e
yitznewton <yitznewton@hotmail.com>
2016-09-27 22:28:23 -0400

    Implement filter when nothing is added

--- lib/bloom_filter.rb
+++ lib/bloom_filter.rb
@@ -1,2 +1,17 @@
+require 'bitarray'
+
 class BloomFilter
+  def initialize(hash_functions, bitmap_size)
+    @hash_functions = hash_functions
+    @bitmap_size = bitmap_size
+    @bitmap = BitArray.new(bitmap_size)
+  end
+
+  def include?(_)
+    false
+  end
+
+  private
+
+  attr_reader :hash_functions, :bitmap_size, :bitmap
 end
--- spec/bloom_filter_spec.rb
+++ spec/bloom_filter_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 require 'bloom_filter'
+require 'hash_function/djb2'
+require 'hash_function/djb2a'
 
 describe BloomFilter do
+  subject { described_class.new(hash_functions, bitmap_size) }
+
+  let(:hash_functions) {
+    [
+      HashFunction::Djb2,
+      HashFunction::Djb2a
+    ]
+  }
+
+  let(:bitmap_size) { 10000 }
+  let(:property_test_runs) { 10000 }
+
+  context 'with an empty filter' do
+    it 'does not recognize any item as included' do
+      property_of {
+        string
+      }.check(property_test_runs) { |item|
+        expect(subject).not_to include(item.dup)
+      }
+    end
+  end
 end
